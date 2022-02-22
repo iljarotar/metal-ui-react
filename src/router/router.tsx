2fad357f349cd11dd366c02d951227a63b69b726
iljarotar <kontakt@ilja-rotar.de>
2022-02-22 16:53:51 +0100

    routes added

---
+++ src/router/router.tsx
@@ -0,0 +1,29 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import BillingSettingsView from '../views/BillingSettingsView'
+import ClusterDetailsView from '../views/ClusterDetailsView'
+import CreateClusterView from '../views/CreateClusterView'
+import DashboardView from '../views/DashboardView'
+import GeneralSettingsView from '../views/GeneralSettingsView'
+import IpAddressesView from '../views/IpAddressesView'
+import KubernetesView from '../views/KubernetesView'
+import VolumesView from '../views/VolumesView'
+import Layout from '../views/Layout'
+
+export default function Router() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<DashboardView />} />
+          <Route path="BillingSettings" element={<BillingSettingsView />} />
+          <Route path="ClusterDetails" element={<ClusterDetailsView />} />
+          <Route path="CreateCluster" element={<CreateClusterView />} />
+          <Route path="IpAddresses" element={<IpAddressesView />} />
+          <Route path="GeneralSettings" element={<GeneralSettingsView />} />
+          <Route path="Kubernetes" element={<KubernetesView />} />
+          <Route path="Volumes" element={<VolumesView />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  )
+}
