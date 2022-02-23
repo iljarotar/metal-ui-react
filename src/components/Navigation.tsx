83abedd5c0d5a3513fced8780c8116f4ff8d467b
iljarotar <kontakt@ilja-rotar.de>
2022-02-23 10:40:28 +0100

    Navigation added

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,10 +1,38 @@
+import '../styles/Navigation.css'
+import { NavLink } from 'react-router-dom'
+
 export default function Navigation() {
-  const style = {
-    gridArea: 'nav'
-  }
   return (
-    <nav style={style}>
-      <h3>Navigation</h3>
+    <nav>
+      <div id="nav-list">
+        <div className="nav-item">
+          <NavLink className={({isActive}) => (isActive?'active':'inactive')} to="/">Dashboard</NavLink>
+        </div>
+        <div className="nav-item">
+          <NavLink className={({isActive}) => (isActive?'active':'inactive')} to="/Kubernetes">Kubernetes</NavLink>
+        </div>
+        <div className="nav-item">
+          <NavLink className={({isActive}) => (isActive?'active':'inactive')} to="/Volumes">Volumes</NavLink>
+        </div>
+        <div className="nav-item">
+          <NavLink className={({isActive}) => (isActive?'active':'inactive')} to="/IpAddresses">Ip Addresses</NavLink>
+        </div>
+        <div>
+          <div id="submenu-summary">Settings</div>
+          <div id="submenu">
+            <div className="nav-item">
+              <NavLink className={({isActive}) => (isActive?'active-submenu-link':'inactive-submenu-link')} to="/GeneralSettings">General</NavLink>
+            </div>
+            <div className="nav-item">
+              <NavLink className={({isActive}) => (isActive?'active-submenu-link':'inactive-submenu-link')} to="/BillingSettings">Billing</NavLink>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div id="version-info">
+        <p>Metal Cloud</p>
+        <p>API Version XY</p>
+      </div>
     </nav>
   )
 }
