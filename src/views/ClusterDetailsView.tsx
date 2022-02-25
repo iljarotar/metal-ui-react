f6ab687555d9f971faee052ef3864342993d971a
iljarotar <kontakt@ilja-rotar.de>
2022-02-25 12:04:38 +0100

    cluster details random todo api call example

--- src/views/ClusterDetailsView.tsx
+++ src/views/ClusterDetailsView.tsx
@@ -1,12 +1,38 @@
+import React from 'react'
 import TopBar from '../components/TopBar'
-import PropTypes from 'prop-types'
-import { useParams } from 'react-router-dom'
+import '../styles/ClusterDetailsView.css'
 
-export default function ClusterDetailsView(props: any) {
-  const { clusterId } = useParams()
-  return ( 
-    <div id="cluster-details">
-      <TopBar title='Cluster Details' subtitle={clusterId} values={true} />
-    </div>
-  )
+interface state {
+  todos: any
+}
+
+export default class ClusterDetailsView extends React.Component<{}, state> {
+  constructor(props: any) {
+    super(props)
+    this.state =  {
+      todos: null
+    }    
+  }
+  async componentDidMount() {
+    const index = Math.floor(Math.random() * 200)
+    const response = await fetch(`https://jsonplaceholder.typicode.com/todos/${index}`)
+    this.setState({
+      todos: await response.json()
+    })
+    console.log(this.state)
+  }
+  render() {
+    return (
+      <div id="cluster-details">
+        <TopBar values={true} title='Cluster Details'/>
+        {this.state.todos?<div id="data">
+          <p><b>User Id: </b>{this.state.todos.userId}</p>
+          <p><b>Id: </b>{this.state.todos.id}</p>
+          <p><b>Title: </b>{this.state.todos.title}</p>
+        </div>:
+        <p>Loading...</p>
+        }
+      </div>
+    )
+  }
 }
